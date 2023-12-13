e8e1d38367ac9763553df09a4be8f1cb205f52d9
psleeee <ellamimy@gmail.com>
2023-12-13 17:13:54 +0800

    12/13 前端修改
    查詢頁面(search.html)目前沒有成功跳到查詢結果的頁面(search_result.html)

--- evaluate/urls.py
+++ evaluate/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'evaluate'
 urlpatterns = [
     path('evaluate_detail/<int:user_id>', views.evaluate_detail, name='evaluate_detail'),
-    path('evaluate_user/<int:user_id>', views.evaluate_buyers, name='evaluate_buyers'),
-    path('evaluate_user/<int:user_id>', views.evaluate_sellers, name='evaluate_sellers'),
-    path('evaluate_write/<str::target_user>', views.write_review, name='write_review'),
+    path('evaluate_user/<int:user_id>/', views.evaluate_user, name='evaluate_user'),
+    path('evaluate_write/<int:user_id>', views.write_review, name='write_review'),
 ]
--- evaluate/views.py
+++ evaluate/views.py
@@ -16,6 +16,11 @@ def evaluate_detail(request, user_id):
     evaluate1 = FakeEvaluate('0001', '0050', 4, '2023/10/03', 'Good!', 3.5)
     evaluate2 = FakeEvaluate('0001', '0100', 5, '2021/01/23', '交貨速度快', 4.5)
     evaluate3 = FakeEvaluate('0001', '7073', 2, '20222/05/24', '二手書品質極差', 3)
+    # Fix the variable names here
+    evaluatedUserID = evaluate1.evaluatedUserID
+    avg_score = evaluate1.avg_score
+    evaluates = [evaluate1, evaluate2, evaluate3]
+    
     return render(request, 'evaluate/evaluate_detail.html', {'user_id': evaluatedUserID, 'avg_score': avg_score, 'evaluates': evaluates})
     
 def evaluate_buyers(request):
@@ -25,6 +30,16 @@ def evaluate_buyers(request):
 def evaluate_sellers(request):
     given_sellers = Order.objects.filter(buyer=request.user).values('seller').distinct()
     return render(request, 'evaluate/evaluate_detail.html', {'given_sellers': given_sellers})
+"""  
+def evaluate_user(request):
+    return render(request, 'evaluate/evaluate_user.html')
+"""  
+def evaluate_user(request, user_id):
+    if request.method == 'POST':
+        # Process form data and save to the database
+        return render(request, 'evaluate/evaluate_user.html', {'user_id': user_id})
+    else:
+        return render(request, 'evaluate/evaluate_user.html', {'user_id': user_id})
     
-def write_review(request, target_user):
-    return render(request, 'evaluate_write.html', {'target_user': target_user})
+def write_review(request, user_id):
+    return render(request, 'evaluate_write.html', {'user_id': user_id})
