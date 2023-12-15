836e123421b22565757b8ff49b95c1ec939fdb5e
biglippppppp <b09207002@ntu.edu.tw>
2023-12-15 16:11:41 +0800

    test

--- main_p/views.py
+++ main_p/views.py
@@ -13,16 +13,15 @@ def index(request, user_id):
 
     user_info = api_response.get('user')
 
-    fake_user = {'user_id': user_id, 'user_name': 'Jerry', 'role': 'user'}
     #輸入假資訊讓網頁顯示
     if request.method == 'POST':
         #改變html中按鈕的value可以達到按下不同按鈕後，跳轉到不同的頁面
         if request.POST.get('button') == 'self_info':
-            return redirect('self_info:index', user_id=fake_user['user_id'])
+            return redirect('self_info:index', user_id=user_id)
         elif request.POST.get('button') == 'sale_order':
-            return redirect('order:sale_order', user_id=fake_user['user_id'])
+            return redirect('order:sale_order', user_id=user_id)
         elif request.POST.get('button') == 'want_order':
-            return redirect('order:want_order', user_id=fake_user['user_id'])
+            return redirect('order:want_order', user_id=user_id)
     return render(request, 'main_p/index.html', {'user_id': user_id, 'user_info': user_info})
     
 def create_order(request, user_id):
